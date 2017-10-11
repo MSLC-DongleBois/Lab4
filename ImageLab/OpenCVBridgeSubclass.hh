b9b9bda36b8b765dcdc7d84a0d9313a17f584b55
Logan Dorsey <logandorsey@gmail.com>
2017-10-11 16:01:33 -0500

    Added subdongle

---
+++ ImageLab/OpenCVBridgeSubclass.hh
@@ -0,0 +1,13 @@
+//
+//  OpenCVBridgeSubclass.h
+//  ImageLab
+//
+//  Created by Logan Dorsey on 10/3/17.
+//  Copyright © 2017 Eric Larson. All rights reserved.
+//
+
+#import "OpenCVBridge.hh"
+
+@interface OpenCVBridgeSubclass : OpenCVBridge
+
+@end
